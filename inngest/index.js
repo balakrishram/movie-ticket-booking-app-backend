af79ed7a32c937fe674792740bd6cb804fa40feb
BALAKRISHNAN <balakrish007007@gmail.com>
2026-01-18 22:19:16 +0530

    added inngest in the project

---
+++ inngest/index.js
@@ -0,0 +1,10 @@
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+
+// Inngest Function to save user data to database
+
+
+export const functions = [];
